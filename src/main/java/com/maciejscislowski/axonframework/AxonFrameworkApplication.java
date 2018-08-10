d76a58f94002abe70d3b0a97c28ed17cff3a4f99
Maciej Scislowski <maciej.scislowski@gmail.com>
2018-08-10 16:42:52 +0200

    initial commit

---
+++ src/main/java/com/maciejscislowski/axonframework/AxonFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.maciejscislowski.axonframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonFrameworkApplication.class, args);
+	}
+}
